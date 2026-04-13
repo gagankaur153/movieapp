0f01b428ba867d1d4110abee63b13f1689f8ae7d
gagankaur153 <gagankaur672000@gmail.com>
2026-04-13 12:28:11 +0530

    fix app.jsx app

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar  from './components/Navbar'
 import MainContent from './components/Home'
 import Sidebar from './components/Sidebar'
-import Moviedetail from './components/Moviedetail'
+// import Moviedetail from './components/Moviedetail'
 import Playvideo from './components/Playvideo'
 import Tvseries from './components/Tvseries'
-import Allepisode from './components/Allepisode'
+// import Allepisode from './components/Allepisode'
 import Tvpop from './components/Tvpop'
 import Footer from './components/Footer'
 
@@ -24,10 +24,10 @@ const   movie  = () => {
     
   <Routes>
     <Route path='/' element={<MainContent/>}/>
-    <Route path='/detail' element={<Moviedetail/>}/>
+    {/* <Route path='/detail' element={<Moviedetail/>}/> */}
     <Route path='/playmovie' element={<Playvideo/>} />
     <Route path='/tvseries' element={<Tvseries/>}/>
-    <Route path='/allepisode' element={<Allepisode/>}/>
+    {/* <Route path='/allepisode' element={<Allepisode/>}/> */}
     <Route path='/tvdetail' element={<Tvpop/>} />
     {/* <Route path='/' element={<Page/>}/> */}
   </Routes>
--- src/components/Playvideo.jsx
+++ src/components/Playvideo.jsx
@@ -8,7 +8,7 @@ const Playvideo = () => {
     // console.log(detail?.id)
     const id = detail?.id
   return (
-    <div>
+    <div className='pb-40'>
     
         { 
   (
